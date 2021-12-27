cce083839338660b62421208869913cb3994d6e2
corneliajohnson <corneliaj91@gmail.com>
2021-12-27 12:28:04 -0600

    added test for message function

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,36 @@ const ganache = require('ganache-cli');
 //Web3 is the soultion to communcitating between a javascript app and the ETH network
 //Web3 is captitalized because it is a construct - just to make instances of the web3 libaray
 const Web3 = require('web3');
-
 //create an instance of web3 and tells the instance to connect to the local test network (ganache)
 const web3 = new Web3(ganache.provider());
+//the complie.js export file contains the interface and bytecode in the :Inbox object
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+const INTITAL_STRING = 'Hi there!'
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  //use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [INTITAL_STRING],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  //test that an address exist for the deployed contract - returns a boolean
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address)
+  });
+
+  it("has a default message", async ()=>{
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, INTITAL_STRING);
+  });
+});
